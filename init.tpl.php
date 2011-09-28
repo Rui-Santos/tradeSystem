53171d116ae5bb66e3d3def32a5f890ca8ae1425
Evgeniy Sokolov <e.sokolov@co.wapstart.ru>
2011-09-29 00:41:06 +0600

    cosmetics in init

--- init.tpl.php
+++ init.tpl.php
@@ -4,20 +4,19 @@
 	error_reporting(E_ALL | E_STRICT);
 	ini_set('display_errors', true);
 
-	define(__NAMESPACE__.'\EWGRA_PROJECTS_DIR', '/home/www/ewgraProjects');
-	define(__NAMESPACE__.'\FRAMEWORK_DIR', EWGRA_PROJECTS_DIR . '/framework');
-	define(__NAMESPACE__.'\BASE_DIR', dirname(__FILE__));
+	define(__NAMESPACE__.'\BASE_DIR', __DIR__);
+	define(__NAMESPACE__.'\FRAMEWORK_DIR', BASE_DIR.'/../framework');
 
 	classesAutoloaderInit();
 
 	function classesAutoloaderInit()
 	{
-		require_once(FRAMEWORK_DIR . '/core/patterns/SingletonInterface.class.php');
-		require_once(FRAMEWORK_DIR . '/core/patterns/Singleton.class.php');
-		require_once(FRAMEWORK_DIR . '/ClassesAutoloader.class.php');
+		require_once(FRAMEWORK_DIR.'/core/patterns/SingletonInterface.class.php');
+		require_once(FRAMEWORK_DIR.'/core/patterns/Singleton.class.php');
+		require_once(FRAMEWORK_DIR.'/ClassesAutoloader.class.php');
 
 		\ewgraFramework\ClassesAutoloader::me()->
-			addSearchDirectory(BASE_DIR . '/classes', 'tradeSystem')->
+			addSearchDirectory(BASE_DIR.'/classes', 'tradeSystem')->
 			addSearchDirectory(FRAMEWORK_DIR, 'ewgraFramework');
 	}
 
